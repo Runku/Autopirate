b34a6e0a4bc3fe96842a28f906a12360dbc480a2
Jot43 <139986366+Jot43@users.noreply.github.com>
2023-09-14 18:36:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,8 +22,8 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/7215c7831b4ae88897448.jpg https://telegra.ph/file/34f3bd6b0cda5bb9b0cca.jpg https://telegra.ph/file/724bdd44693500c024ec7.jpg https://telegra.ph/file/1b39388dd4a343cc325bc.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/Not-Found-09-14-2")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/Welcome-09-12-12")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
