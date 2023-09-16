8fbefdea70d66c12cc4bf746a2f69ba7b8fcbac6
Jot43 <139986366+Jot43@users.noreply.github.com>
2023-09-16 16:20:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/Jot43/AutoFilterPaidV2.git /Auto-search-tamil-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /AutoFilterPaidV2
 fi
 cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
